9a1218047cef5395af5e5c7dbc43be2f1dde32b0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-20 11:59:27 +0100

    correzione length - 1; correzione bool risposte;

--- LibreriaArray/GestioneArray.cs
+++ LibreriaArray/GestioneArray.cs
@@ -8,7 +8,7 @@ namespace LibreriaArray
 
         public static void LeggiArrayInt(int[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
                 array[i] = int.Parse(Console.ReadLine());
@@ -16,7 +16,7 @@ namespace LibreriaArray
         }
         public static void StampaArrayInt(int[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
             }
@@ -24,7 +24,7 @@ namespace LibreriaArray
         public static int[] CopiaArrayInt(int [] array)
         {
             int[] array2 = new int[array.Length];
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array2[i] = array[i];
             }
@@ -32,38 +32,32 @@ namespace LibreriaArray
         }
         public static bool RicercaArrayInt(int [] array, int elemento)
         {
-            int v = 0;
-            for (int i = 0; i < array.Length - 1; i++)
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == elemento)
-                    v++;
+                    trovato = true;
             }
-            if (v == 0)
-                return false;
-            else
-                return true;
+            return trovato;
         }
         public static bool SonoUgualiInt(int [] array1, int [] array2)
         {
             if (array1.Length != array2.Length)
                 return false;
-            string risposta = "si";
-            for (int i = 0; i < array1.Length - 1; i++)
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
             {
                 if (array1[i] != array2[i])
-                    risposta = "no";
+                    risposta = false;
             }
-            if (risposta == "si")
-                return true;
-            else
-                return false;
+            return risposta;
         }
 
         // Tipi double
 
         public static void LeggiArrayDouble(double[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
                 array[i] = double.Parse(Console.ReadLine());
@@ -71,7 +65,7 @@ namespace LibreriaArray
         }
         public static void StampaArrayDouble(double[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
             }
@@ -79,7 +73,7 @@ namespace LibreriaArray
         public static double[] CopiaArrayDouble(double[] array)
         {
             double[] array2 = new double [array.Length];
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array2[i] = array[i];
             }
@@ -87,38 +81,32 @@ namespace LibreriaArray
         }
         public static bool RicercaArrayDouble(double[] array, double elemento)
         {
-            int v = 0;
-            for (int i = 0; i < array.Length - 1; i++)
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == elemento)
-                    v++;
+                    trovato = true;
             }
-            if (v == 0)
-                return false;
-            else
-                return true;
+            return trovato;
         }
         public static bool SonoUgualiDouble(double[] array1, double[] array2)
         {
             if (array1.Length != array2.Length)
                 return false;
-            string risposta = "si";
-            for (int i = 0; i < array1.Length - 1; i++)
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
             {
                 if (array1[i] != array2[i])
-                    risposta = "no";
+                    risposta = false;
             }
-            if (risposta == "si")
-                return true;
-            else
-                return false;
+            return risposta;
         }
 
         // Tipi string
 
         public static void LeggiArrayString(string[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine($"Inserisci il valore dell'array i posizione {1}");
                 array[i] = Console.ReadLine();
@@ -126,7 +114,7 @@ namespace LibreriaArray
         }
         public static void StampaArrayString(string[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
             }
@@ -134,7 +122,7 @@ namespace LibreriaArray
         public static string[] CopiaArrayString(string[] array)
         {
             string[] array2 = new string[array.Length];
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array2[i] = array[i];
             }
@@ -142,31 +130,25 @@ namespace LibreriaArray
         }
         public static bool RicercaArrayString(string[] array, string elemento)
         {
-            int v = 0;
-            for (int i = 0; i < array.Length - 1; i++)
+            bool trovato = false;
+            for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == elemento)
-                    v++;
+                    trovato = true;
             }
-            if (v == 0)
-                return false;
-            else
-                return true;
+            return trovato;
         }
         public static bool SonoUgualiString(string[] array1, string[] array2)
         {
             if (array1.Length != array2.Length)
                 return false;
-            string risposta = "si";
-            for (int i = 0; i < array1.Length - 1; i++)
+            bool risposta = true;
+            for (int i = 0; i < array1.Length; i++)
             {
                 if (array1[i] != array2[i])
-                    risposta = "no";
+                    risposta = false;
             }
-            if (risposta == "si")
-                return true;
-            else
-                return false;
+            return risposta;
         }
 
         // Altri metodi
@@ -174,7 +156,7 @@ namespace LibreriaArray
         public static int CercaMax(int[] array)
         {
             int max = int.MinValue;
-            for (int i = 0; i < array.Length - 1;)
+            for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] > max)
                     max = array[i];
@@ -184,38 +166,32 @@ namespace LibreriaArray
         public static int CercaMin(int[] array)
         {
             int min = int.MaxValue;
-            for (int i = 0; i < array.Length - 1;)
+            for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] > min)
+                if (array[i] < min)
                     min = array[i];
             }
             return min;
         }
         public static bool Crescente (int [] array)
         {
-            string risposta = "si";
+            bool risposta = true;
             for ( int i = 1; i < array.Length; i++)
             {
                 if (array[i] <= array[i - 1])
-                    risposta = "no";
+                    risposta = false;
             }
-            if (risposta == "si")
-                return true;
-            else
-                return false;
+            return risposta;
         }
         public static bool Decrescente(int[] array)
         {
-            string risposta = "si";
+            bool risposta = true;
             for (int i = 1; i < array.Length; i++)
             {
                 if (array[i] >= array[i - 1])
-                    risposta = "no";
+                    risposta = false;
             }
-            if (risposta == "si")
-                return true;
-            else
-                return false;
+            return risposta;
         }
     }
 }
